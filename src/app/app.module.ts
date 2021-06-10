b1383f6949500f538406f10571e97310e1b7d4db
Wopnis <wopnis2016@outlook.com>
2021-06-10 20:51:39 +0300

    start project New Hacker news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { CommentsPageComponent } from './comments-page/comments-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    NewsPageComponent,
+    CommentsPageComponent
   ],
   imports: [
     BrowserModule,
